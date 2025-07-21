f7cf313741cd128db0ceeb2bce17db6b5044de71
doseofamina <99176647+doseofamina@users.noreply.github.com>
2025-07-21 10:22:59 +0100

    Update streamlit_app.py
    Updated the title and subheader

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,12 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
+st.title("My PArents New Healthy Diner")
+st.subheader("Breakfast Menu")
 st.write(
-  """Choose the fruits you want in your custom Smoothie!
+  """Omega 3 & Blueberry Oatmeal \n
+  Kale, Spinach & Rocket Smoothie \n
+  Hard-Boiled Free-Range Egg
   """
 )
 
